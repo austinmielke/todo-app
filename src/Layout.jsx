633c9830b0a5ff950f2dc121014bded88d09778e
austinmielke <amielkedev@gmail.com>
2023-09-20 09:38:32 -0500

    fix: makes layout more responsive for smaller screens

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,8 +1,8 @@
 const Layout = ({ children }) => {
   return (
-    <div className="flex h-screen items-center justify-center bg-base-300">
-      <div className="flex w-1/2 flex-col">
-        <h1 className="-mt-12 mb-8 text-center text-3xl font-bold text-success">
+    <div className="flex h-screen justify-center bg-base-300 md:items-center">
+      <div className="flex w-full flex-col md:w-1/2">
+        <h1 className="mb-8 mt-2 text-center text-3xl font-bold text-success md:-mt-12">
           Todo App
         </h1>
         <div className="card w-full bg-base-100 shadow-xl">
